caba122e2dd534210bfdca5e5c00b13c943826dd
sxrose <53537708+ky1e-cyber@users.noreply.github.com>
2025-02-12 16:00:12 +0700

    Removing distfiles tar after download

--- download-distfiles-backup.sh
+++ download-distfiles-backup.sh
@@ -3,6 +3,6 @@
 mkdir distfiles &&
   cd distfiles &&
   curl -L -O https://github.com/Trusted-Compiler/distfiles/releases/download/4eceb77/distfiles.tar.gz &&
-  tar -xvf distfiles.tar.gz &&
+  tar -xvf distfiles.tar.gz && rm distfiles.tar.gz &&
   cd .. && ./download-distfiles.sh
     
