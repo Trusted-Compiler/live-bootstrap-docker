54561405b0507ffca1f9b821810f7a7c7ff8e366
sxrose <53537708+ky1e-cyber@users.noreply.github.com>
2025-02-12 16:12:59 +0700

    Bump distfiles backup to f169d99

--- download-distfiles-backup.sh
+++ download-distfiles-backup.sh
@@ -2,7 +2,7 @@
 
 mkdir distfiles &&
   cd distfiles &&
-  curl -L -O https://github.com/Trusted-Compiler/distfiles/releases/download/4eceb77/distfiles.tar.gz &&
+  curl -L -O https://github.com/Trusted-Compiler/distfiles/releases/download/f169d99/distfiles.tar.gz &&
   tar -xvf distfiles.tar.gz && rm distfiles.tar.gz &&
   cd .. && ./download-distfiles.sh
     
